3139f22d3e6af511b09786346ba7c00d8b9907f6
azakg <azakgzuu@gmail.com>
2024-07-11 17:08:20 -0400

    modifyhing

---
+++ Python_Crash/Chapet#3/modifying_list.py
@@ -0,0 +1,12 @@
+motocycles = ['yamaha', 'honda', 'suzuki']
+
+print(motocycles)
+#modify list
+motocycles[0] = 'dukati'
+print(motocycles)
+
+motocycles.append('yamaha')
+
+print(motocycles)
+
+motocycles.insert(0, 'azamat')
