b025a1f7e50650e27a0bd0f1e955721718d8370a
azakg <azakgzuu@gmail.com>
2024-07-11 18:50:17 -0400

    range

--- Python_Crash/Chapter#4/range.py
+++ Python_Crash/Chapter#4/range.py
@@ -1,2 +1,21 @@
 for value in range(1, 5):
     print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+print(squares)
+
+numbers1 = []
+for value in range(1, 11):
+    numbers1.append(value)
+print(min(numbers1))
+print(max(numbers1))
+print(sum(numbers1))
