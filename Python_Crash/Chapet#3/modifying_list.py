d125acb91b403660759fdd1b673519f6613f7a71
azakg <azakgzuu@gmail.com>
2024-07-11 17:40:35 -0400

    modifyhing

--- Python_Crash/Chapet#3/modifying_list.py
+++ Python_Crash/Chapet#3/modifying_list.py
@@ -2,11 +2,11 @@ motocycles = ['yamaha', 'honda', 'suzuki']
 
 print(motocycles)
 #modify list
-motocycles[0] = 'dukati'
+motocycles[0] = 'dukati' #change element 0 from 'yamaha' to 'dukati'
 print(motocycles)
 
-motocycles.append('yamaha')
+motocycles.append('yamaha') # add 'yamaha' to the end of the list
 
 print(motocycles)
 
-motocycles.insert(0, 'azamat')
+motocycles.insert(0, 'azamat') # insert azamat to the specific location inside of the list, in the is case to 0
