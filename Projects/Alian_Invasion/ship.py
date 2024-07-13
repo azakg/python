d1dd3f00b70fa7574f62d3a6a198038920fbb278
azakg <azakgzuu@gmail.com>
2024-07-13 13:03:57 -0400

    Update Continuous Movement

--- Projects/Alian_Invasion/ship.py
+++ Projects/Alian_Invasion/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game1):
         """Intitialize the ship and its starting position."""
         self.screen = ai_game1.screen
+        self.settings = ai_game1.settings
         self.screen_rect = ai_game1.screen.get_rect()
 
         #Load the ship image and get its rect.
@@ -16,16 +17,22 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
         #Movement flag: start with a ship that's not moving
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag"""
-        if self.moving_right:
-            self.rect.x +=3
-        if self.moving_left:
-            self.rect.x -=3
+        #Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
