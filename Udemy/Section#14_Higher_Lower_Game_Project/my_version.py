1a39e29250628cb80dd9d8c42418884ba3e7a042
azakg <azakgzuu@gmail.com>
2024-07-29 11:32:46 -0400

    High low game, My version

---
+++ Udemy/Section#14_Higher_Lower_Game_Project/my_version.py
@@ -0,0 +1,64 @@
+from art import logo
+from art import vs
+import random
+from game_data import data
+
+# ask_and_check
+def ask():
+    print(logo)
+    rand_a = random.randint(1, 50)
+    rand_b = random.randint(1, 50)
+    print(f"Compare A: {data[rand_a]['name']}, a {data[rand_a]['description']}, from {data[rand_a]['country']}")
+    print(vs)
+    print(f"Compare B: {data[rand_b]['name']}, a {data[rand_b]['description']}, from {data[rand_b]['country']}")
+    print(data[rand_a]['follower_count'])
+    print(data[rand_b]['follower_count'])
+    guess = input("Who has more followers? Type 'A' or 'B': ")
+    return check(rand_a, rand_b, guess)
+
+def ask2(score):
+    print(logo)
+    rand_a = random.randint(1, 50)
+    rand_b = random.randint(1, 50)
+    print(f"Your2 score is {score}")
+    print(f"Compare A: {data[rand_a]['name']}, a {data[rand_a]['description']}, from {data[rand_a]['country']}")
+    print(vs)
+    print(f"Compare B: {data[rand_b]['name']}, a {data[rand_b]['description']}, from {data[rand_b]['country']}")
+    print(data[rand_a]['follower_count'])
+    print(data[rand_b]['follower_count'])
+    guess = input("Who has more followers? Type 'A' or 'B': ")
+    return check(rand_a, rand_b, guess)
+
+# Check guess
+def check(rand_a, rand_b, guess):
+   rand_a = data[rand_a]['follower_count']
+   rand_b = data[rand_b]['follower_count']
+   winner = 'a'
+   if rand_a > rand_b:
+        winner = 'a'
+   else:
+        winner = 'b'
+   if winner == guess:
+        return True
+
+   else:
+        return False
+
+def game():
+    score = 0
+    tmp = 0
+    result = True
+    while True:
+        if score > tmp:
+            result = ask2(score)
+            tmp = score
+        else:
+            result = ask()
+        if not result:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            break
+        else:
+            score += 1
+            print(f"Your score is {score}")
+
+game()
