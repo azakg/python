ca643e2c22e26cdd19ead80026bf773a424caecf
azakg <azakgzuu@gmail.com>
2024-07-31 10:56:36 -0400

    Section#16

---
+++ Udemy/Section#16_OOP/main.py
@@ -0,0 +1,18 @@
+# import another_module
+#
+# print(another_module.another_varible)
+#
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("darkSeaGreen")
+# timmy.forward(100)
+# timmy.right(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
