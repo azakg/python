8e453dfaa687efe4aad041ea39ed9c41bc314eef
azakg <azakgzuu@gmail.com>
2024-07-31 15:31:00 -0400

    Section#16 Building_Coffee_Machine initial files

--- Udemy/Section#16_OOP/main.py
+++ Udemy/Section#16_OOP/main.py
@@ -16,3 +16,16 @@
 # my_screen = Screen()
 # print(my_screen.canvheight)
 # my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+print(table)
+
+
+# table.field_names = ["Azamat", "Nurzhamal", "Tamchy", "Altai"]
+# table.add_row([38, 33, 9, 5])
+# print(table)
