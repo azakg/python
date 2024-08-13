2f9fb78531e8d6315435b35a875c2acb8ea2770e
azakg <azakgzuu@gmail.com>
2024-08-13 13:12:17 -0400

    Section#18 Learning Turtle

---
+++ Udemy/Section#18_Turtle_and_the_Graphical_User_Interface(GUI)/main.py
@@ -0,0 +1,27 @@
+import turtle
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("cyan")
+
+for _ in range(0, 4):
+    tim.forward(100)
+    tim.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
