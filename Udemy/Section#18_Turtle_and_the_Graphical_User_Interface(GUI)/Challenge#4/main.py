b6c8c0ef94688a21ab377cb58c13ca862a1415a1
azakg <azakgzuu@gmail.com>
2024-08-14 13:38:52 -0400

    Section#18 Challenge#4

--- Udemy/Section#18_Turtle_and_the_Graphical_User_Interface(GUI)/Challenge#4/main.py
+++ Udemy/Section#18_Turtle_and_the_Graphical_User_Interface(GUI)/Challenge#4/main.py
@@ -1,15 +1,12 @@
 import turtle as t
 import random
 
-
-
 tim = t.Turtle()
-
 
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGrey", "SeaGreen"]
 directions = [0, 90, 180, 270]
 tim.pensize(5)
-tim.speed("fastest")
+tim.speed("slowest")
 for _ in range(200):
     tim.forward(30)
     tim.setheading(random.choice(directions))
