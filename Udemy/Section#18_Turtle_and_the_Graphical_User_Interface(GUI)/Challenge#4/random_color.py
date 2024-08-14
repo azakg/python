958a3964e500dd216a9eaaf06d6f489234c97029
azakg <azakgzuu@gmail.com>
2024-08-14 14:04:54 -0400

    Section#18 Challenge#4 Random color Done

--- Udemy/Section#18_Turtle_and_the_Graphical_User_Interface(GUI)/Challenge#4/random_color.py
+++ Udemy/Section#18_Turtle_and_the_Graphical_User_Interface(GUI)/Challenge#4/random_color.py
@@ -2,12 +2,18 @@ import turtle as t
 import random
 
 tim = t.Turtle()
+t.colormode(255)
 
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGrey", "SeaGreen"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
 directions = [0, 90, 180, 270]
 tim.pensize(5)
 tim.speed("slowest")
 for _ in range(200):
     tim.forward(30)
     tim.setheading(random.choice(directions))
-    tim.color(random.choice(colors))
+    tim.color(random_color())
