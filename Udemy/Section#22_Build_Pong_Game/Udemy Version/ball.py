c8bc013295d4da6257a6520f127b6215b1d1515a
azakg <azakgzuu@gmail.com>
2024-08-22 10:44:25 -0400

    Section#22 Udemy Version Finall

--- Udemy/Section#22_Build_Pong_Game/Udemy Version/ball.py
+++ Udemy/Section#22_Build_Pong_Game/Udemy Version/ball.py
@@ -1,10 +1,6 @@
 from turtle import Turtle
 import random
 
-x_variable = 10
-y_variable = 10
-
-signs = [10, -10]
 
 
 class Ball(Turtle):
@@ -13,30 +9,26 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
 
     def move(self):
-
-        new_x = self.xcor()
-        new_y = self.ycor()
-
-        if new_y == 280:
-            new_y = self.ycor() - y_variable
-            signs[1] = -10
-        elif new_y == -280:
-            new_y = self.ycor() + y_variable
-            signs[1] = 10
-        elif new_x == 380:
-            new_x = self.xcor() - x_variable
-            signs[0] = -10
-        elif new_x == -380:
-            new_x = self.xcor() + x_variable
-            signs[0] = 10
-        else:
-            new_x = self.xcor() + signs[0]
-            new_y = self.ycor() + signs[1]
-
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed * 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
 
 
--- Udemy/Section#22_Build_Pong_Game/Udemy Version/paddle.py
+++ Udemy/Section#22_Build_Pong_Game/Udemy Version/paddle.py
@@ -12,7 +12,6 @@ class Paddle(Turtle):
     def go_up(self):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
-        print('UP')
 
     def go_down(self):
         new_y = self.ycor() - 20
