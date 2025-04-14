8e49fa8c37c0eb3e4de081e5cb896e2fa78842a9
azakg <azakgzuu@gmail.com>
2025-04-14 06:00:52 -0400

    smpt2

--- Section_32_Smpt/Birthday Wisher (Day 32) start/main.py
+++ Section_32_Smpt/Birthday Wisher (Day 32) start/main.py
@@ -6,5 +6,8 @@ password = "fxsksfeulwbyicjd"
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="nurjamalkanatbekova8@gmail.com", msg="Hello_from_Python")
+connection.sendmail(
+    from_addr=my_email,
+    to_addrs="nurjamalkanatbekova8@gmail.com",
+    msg="Subject:Hello\n\nThis is the body of my email.")
 connection.close()
