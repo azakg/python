3788141bb26c04b84fc8798620aa80637b29f071
azakg <azakgzuu@gmail.com>
2025-04-13 22:11:55 -0400

    smpt

---
+++ Section_32_Smpt/Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,10 @@
+import smtplib
+
+my_email = "azakgzuu@gmail.com"
+password = "fxsksfeulwbyicjd"
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="nurjamalkanatbekova8@gmail.com", msg="Hello_from_Python")
+connection.close()
