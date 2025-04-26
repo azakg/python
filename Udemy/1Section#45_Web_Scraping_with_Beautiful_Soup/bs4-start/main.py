8703dbff9a0a581faa40736f3476fd9355ef3bb3
azakg <azakgzuu@gmail.com>
2025-04-26 07:07:20 -0400

    selector

--- Udemy/1Section#45_Web_Scraping_with_Beautiful_Soup/bs4-start/main.py
+++ Udemy/1Section#45_Web_Scraping_with_Beautiful_Soup/bs4-start/main.py
@@ -5,9 +5,9 @@ with open("website.html", encoding="utf8") as file:
 
 soup = BeautifulSoup(contents, "html.parser")
 
-all_ancor_tags = soup.find_all(name = "a")
+all_anchor_tags = soup.find_all(name = "a")
 
-for tag in all_ancor_tags:
+for tag in all_anchor_tags:
     str1 = str(tag)
     str_mutch = "https://angelabauer.github.io/cv/hobbies.html"
     if str_mutch in str1:
@@ -17,3 +17,11 @@ h1_item = soup.find(name = "h1", id = 'name')
 print(h1_item)
 h1_item1 = soup.find(name = "h1", id = "name1")
 print(h1_item1)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
+
+name = soup.select_one(selector="#name")
+print(name)
+name1 = soup.select_one(selector="#name1")
+print(name1)
