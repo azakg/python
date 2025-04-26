84cb648ed3d1527a6df8407995ffcca265876593
azakg <azakgzuu@gmail.com>
2025-04-25 23:18:48 -0400

    find_all/find()

--- Udemy/1Section#45_Web_Scraping_with_Beautiful_Soup/bs4-start/main.py
+++ Udemy/1Section#45_Web_Scraping_with_Beautiful_Soup/bs4-start/main.py
@@ -4,4 +4,16 @@ with open("website.html", encoding="utf8") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, "html.parser")
-print(soup.li)
+
+all_ancor_tags = soup.find_all(name = "a")
+
+for tag in all_ancor_tags:
+    str1 = str(tag)
+    str_mutch = "https://angelabauer.github.io/cv/hobbies.html"
+    if str_mutch in str1:
+        print(tag.get("href"))
+
+h1_item = soup.find(name = "h1", id = 'name')
+print(h1_item)
+h1_item1 = soup.find(name = "h1", id = "name1")
+print(h1_item1)
