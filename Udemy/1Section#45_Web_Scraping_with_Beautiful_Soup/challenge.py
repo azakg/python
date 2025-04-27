16a7dcce9b095b1c56e5d50789419f8446ba8c82
azakg <azakgzuu@gmail.com>
2025-04-27 07:39:21 -0400

    Web Scraping#2

--- Udemy/1Section#45_Web_Scraping_with_Beautiful_Soup/challenge.py
+++ Udemy/1Section#45_Web_Scraping_with_Beautiful_Soup/challenge.py
@@ -2,5 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 
 response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
 
-print(response.text)
+soup = BeautifulSoup(yc_web_page, "html.parser")
+print(soup.title)
+article_tag = soup.find(name="span", class_="titleline")
+article_text = article_tag.getText()
+#article_link = article_text
+print(article_text)
