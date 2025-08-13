e79b11cb6cdd19ad162e098cfe46591939217962
azakg <azakgzuu@gmail.com>
2025-08-13 11:54:51 -0400

    init

--- Python_Crash/Chapter#4/looping.py
+++
@@ -1,6 +0,0 @@
-magicians = ['alice', 'david', 'carolina']
-for magican in magicians:
-    print(f"{magican.title()}, that was a great trick!")
-    print(f"I can't wait to see your next trick, P{magican.title()}.\n")
-
-print("Than you, everyone. That was a great magic show!")
--- Python_Crash/Chpater#6_Dictonary/remove_key_value_pair.py
+++
@@ -1,4 +0,0 @@
-alien_0 = {'color':'green', 'poinst': 5}
-print(alien_0)
-del alien_0['poinst']
-print(alien_0)
