e9aa64053143154401db751619dacb640b00239a
azakg <azakgzuu@gmail.com>
2025-08-23 12:19:11 -0400

    random and list

---
+++ Udemy/Section#4_Randomisation_and_Python_list/test.py
@@ -0,0 +1,25 @@
+import random
+# import my_module
+#
+# my_list = [1,5,6,3,12,67]
+#
+# rand_num = random.randint(1, 10)
+# print(rand_num)
+#
+# rand_choice = random.choice(my_list)
+# print(rand_choice)
+#
+# print(my_module.my_favorite_number)
+#
+# rand1 = random.random() * 10
+# print(rand1)
+
+# rand1_or2 = random.randint(1,2)
+# if rand1_or2 == 1:
+#     print("Head")
+# elif rand1_or2 == 2:
+#     print("Tails")
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america[random.randint(0,50)])
