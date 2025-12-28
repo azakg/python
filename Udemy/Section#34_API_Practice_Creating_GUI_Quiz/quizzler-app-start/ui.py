124b2da5527cbd55b33ca2938cd93b73a221acc5
azakg <azakgzuu@gmail.com>
2025-12-28 14:49:53 -0500

    122825

---
+++ Udemy/Section#34_API_Practice_Creating_GUI_Quiz/quizzler-app-start/ui.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150, 125,
+            text="Some question text",
+            fill=THEME_COLOR,
+            width=280,
+            font=("Arial", 16, "italic")
+        )
+
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+# QuizInterface()
+
+# root = Tk()
+# w = Label(root, text='GeeksForGeeks.org!')
+# w.pack()
+# root.mainloop()
