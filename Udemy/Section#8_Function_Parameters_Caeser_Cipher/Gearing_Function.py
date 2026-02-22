0f8db02f8801915db185694f0706d9af4f4ee75c
azakg <azakgzuu@gmail.com>
2026-02-22 14:34:36 -0500

    Parameters in Function

--- Udemy/Section#8_Function_Parameters_Caeser_Cipher/Gearing_Function.py
+++ Udemy/Section#8_Function_Parameters_Caeser_Cipher/Gearing_Function.py
@@ -1,13 +1,22 @@
-def greating(text):
+# def greating(text):
+#
+#     if int(text) == 1:
+#         print("You typed: 1")
+#     elif int(text) == 2:
+#         print("You typed: 2")
+#     else:
+#         print("You need to type 1 or 2")
+#
+# Text = ""
+# while Text != quit:
+#     Text = input("Please type some text: ")
+#     greating(Text)
 
-    if int(text) == 1:
-        print("You typed: 1")
-    elif int(text) == 2:
-        print("You typed: 2")
-    else:
-        print("You need to type 1 or 2")
+#Function with more that 1 input
 
-Text = ""
-while Text != quit:
-    Text = input("Please type some text: ")
-    greating(Text)
+def great_with(name, location):
+    #name = name
+    #location = location
+    print(f"Hello {name}, your location is {location}")
+
+great_with( location = "North Olmsted", name ="Azamat")
